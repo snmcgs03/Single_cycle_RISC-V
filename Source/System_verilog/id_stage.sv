d46e63f5104acab0244ae5e5599b535c9b99131c
SHASHANK NADIG M <156205050+snmcgs03@users.noreply.github.com>
2025-07-19 22:25:48 +0530

    id_stage.sv

--- Source/System_verilog/id_stage.sv
+++ Source/System_verilog/id_stage.sv
@@ -12,7 +12,7 @@ output logic fn7_5,branch,mem_read,mem_write,alu_src;
 (* KEEP = "TRUE" *) wire [4:0]rs1,rs2,rd;
 (* KEEP = "TRUE" *) wire [19:0]imm_uj_outd;
 (* KEEP = "TRUE" *) wire [11:0]imm_out_d;
-(* DONT_TOUCH = "TRUE" *) decoder #(32) decode (.imm11_5(imm11_5),.instruction(instruction),.rs1(rs1),.rs2(rs2),.rd(rd),.imm(imm_out_d),.imm_uj(imm_uj_outd),.fn3(fn3_out_d),.opcode(opcode_out_d),.fn7_5(fn7_5));
+(* DONT_TOUCH = "TRUE" *) decoder decode (.imm11_5(imm11_5),.instruction(instruction),.rs1(rs1),.rs2(rs2),.rd(rd),.imm(imm_out_d),.imm_uj(imm_uj_outd),.fn3(fn3_out_d),.opcode(opcode_out_d),.fn7_5(fn7_5));
 (* DONT_TOUCH = "TRUE" *) imm_generator imm_gen (.imm_input_uj(imm_uj_outd),.imm_input(imm_out_d),.imm_output(imm_out),.opcode(opcode_out_d),.fn3(fn3_out_d));
 (* DONT_TOUCH = "TRUE" *) reg_file register_file (.rd_sel(rd),.reg_write(reg_write),.wb_data(wb_data),.clk(clk),.reset(reset),.rs1_sel(rs1),.rs2_sel(rs2),.rs1_data(rs1_data),.rs2_data(rs2_data));
 (* DONT_TOUCH = "TRUE" *) main_control mc (.mux_inp(mux_inp),.opcode(opcode_out_d),.branch(branch),.memread(mem_read),.memtoreg(memtoreg),.aluop(aluop),.memwrite(mem_write),.alusrc(alu_src),.reg_write(reg_write));
